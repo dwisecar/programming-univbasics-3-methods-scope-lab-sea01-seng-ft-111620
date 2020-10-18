17e51a25ea31d5bf81b19de4714499c25e353a2e
David Wisecarver <dwisecar@gmail.com>
2020-10-18 16:00:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,3 +9,14 @@ status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def toadstool
   puts status
 end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
+def all_phrases
+  puts mario
+  puts toadstool
+  puts link
+end
