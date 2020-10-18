c8ac0fa8e1786aab37379070d108a5763a5f0196
David Wisecarver <dwisecar@gmail.com>
2020-10-18 16:01:27 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,9 +4,10 @@ def mario
   puts phrase
 end
 
-$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
